45a0e8e784317ed4573ae3d5ff0ce04019d5dd6c
Benoit Pingris <benoit.pingris@epitech.eu>
2018-02-27 22:00:58 +0100

    handle error

--- 202unsold.rb
+++ 202unsold.rb
@@ -87,6 +87,18 @@ def getLawY(table)
 	return table
 end
 
+def displayZlaw(array)
+	z = [array[0][0],
+	array[0][1] + array[1][0],
+	array[0][2] + array[2][0] + array[1][1],
+	array[0][3] + array[3][0] + array[1][2] + array[2][1],
+	array[0][4] + array[4][0] + array[1][3] + array[3][1] + array[2][2],
+	array[1][4] + array[4][1] + array[3][2] + array[2][3],
+	array[2][4] + array[4][2] + array[3][3],
+	array[3][4] + array[4][3],
+	array[4][4]]
+end
+
 def myUnsold(a, b)
 	
 	table = Array.new(6) {Array.new(6, 0)}
@@ -106,6 +118,7 @@ def myUnsold(a, b)
 	y=0
 	table = getLawY(table)
 	displayTable(table)
+	displayZlaw(table)
 end
 
 def main()
